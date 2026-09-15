fde44a2138826d7d86f3302a9ed42281107f534f
Audri Melina <macbookairm2@Audri-Melina.local>
2026-09-15 17:45:41 +0700

    Force light mode default

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,8 +26,11 @@
             }
         }
         // Init dark mode immediately to prevent flash
-        if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        if (localStorage.getItem('theme') === 'dark') {
             document.documentElement.classList.add('dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+            document.documentElement.classList.remove('dark');
         }
     </script>
     <style>
